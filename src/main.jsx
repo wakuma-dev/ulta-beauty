3d666110a7e03d0bf730b0668e1bd96bd58e6330
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-05-21 15:01:26 +0300

    feat(deploy): add gh-pages deployment setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ulta-beauty">
       <App />
     </BrowserRouter>
   </StrictMode>,
